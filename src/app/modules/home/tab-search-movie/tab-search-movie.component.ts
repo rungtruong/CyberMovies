3d445c8b3779c7e53be78dd95a092faf70537f7e
Forest Truong <forest.truong@styl.solutions>
2019-03-24 00:28:18 +0700

    fix tab search movie

--- src/app/modules/home/tab-search-movie/tab-search-movie.component.ts
+++ src/app/modules/home/tab-search-movie/tab-search-movie.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { MovieService } from 'src/app/_core/services/movie.service';
-
+import { formatDate } from '@angular/common';
 @Component({
   selector: 'app-tab-search-movie',
   templateUrl: './tab-search-movie.component.html',
   styleUrls: ['./tab-search-movie.component.scss']
 })
 export class TabSearchMovieComponent implements OnInit {
+  comingsoon = "Phim sắp chiếu";
   movies: any;
+  theaters: Array<any> = [];
+  dates: Array<any> = [];
+  times: Array<any> = [];
   selectmovieDetail: any = {};
-  movieCode: any;
   constructor(private movieService: MovieService) { }
 
   ngOnInit() {
@@ -20,11 +23,39 @@ export class TabSearchMovieComponent implements OnInit {
       this.movies = data;
     })
   }
-  movieDetail() {
-    this.movieService.chiTietPhim(this.movieCode).subscribe(data => {
+  movieDetail(movieCode) {
+    this.theaters = [];
+    this.dates = [];
+    this.movieService.chiTietPhim(movieCode).subscribe(data => {
       this.selectmovieDetail = data;
-      console.log(this.selectmovieDetail);
+      if (this.selectmovieDetail.LichChieu != "") {
+        let theater = this.selectmovieDetail.LichChieu[0].MaRap;
+        let dateTime = this.selectmovieDetail.LichChieu[0].NgayChieuGioChieu;
+        for (let i = 0; i < this.selectmovieDetail.LichChieu.length; i++) {
+          if (theater != this.selectmovieDetail.LichChieu[i].MaRap) {
+            this.theaters.push(this.selectmovieDetail.LichChieu[i]);
+          }
+          if (dateTime != this.selectmovieDetail.LichChieu[i].NgayChieuGioChieu) {
+            this.dates.push(this.selectmovieDetail.LichChieu[i].NgayChieuGioChieu);
+          }
+        }
+        this.theaters.push(this.selectmovieDetail.LichChieu[0]);
+        this.dates.push(this.selectmovieDetail.LichChieu[0].NgayChieuGioChieu);
+      }
+      console.log(this.theaters);
     })
+  }
+  movieTimeShow(time) {
+    this.times = [];
+    for (let i = 0; i < this.dates.length; i++) {
+      let currentTime = formatDate(this.dates[i], 'H:mm', 'en-US', '+07');
+      if (time != currentTime) {
+        this.times.push(currentTime);
+      }
+    }
+    this.times.push(time);
+    // console.log(this.times);
+
   }
 
 }
