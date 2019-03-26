d1a85813e4a4352ffa0a949c7e467a41d2c5c05a
Forest Truong <forest.truong@styl.solutions>
2019-03-26 17:44:34 +0700

    fix carousel img

--- src/app/modules/home/carousel/carousel.component.ts
+++ src/app/modules/home/carousel/carousel.component.ts
@@ -13,14 +13,12 @@ export class CarouselComponent implements OnInit, AfterViewInit {
   isShowModal: boolean;
   currentUrl: any;
   dsPhim: any = [
-    { HinhAnh: "../../../../assets/img/slide1.jpg", Trailer: "https://www.youtube.com/watch?v=IIIgZI8QDc8" },
-    { HinhAnh: "../../../../assets/img/slide2.jpg", Trailer: "https://www.youtube.com/watch?v=3T9c42gxXzg" },
-    { HinhAnh: "../../../../assets/img/slide3.jpg", Trailer: "https://www.youtube.com/watch?v=IIIgZI8QDc8" },
-    { HinhAnh: "../../../../assets/img/slide4.jpg", Trailer: "https://www.youtube.com/watch?v=IIIgZI8QDc8" },
+    { HinhAnh: "./assets/img/slide1.jpg", Trailer: "https://www.youtube.com/watch?v=6zQe-K8Ywq4" },
+    { HinhAnh: "./assets/img/slide2.jpg", Trailer: "https://www.youtube.com/watch?v=0G8EHFK6kGo" },
+    { HinhAnh: "./assets/img/slide3.jpg", Trailer: "https://www.youtube.com/watch?v=IIIgZI8QDc8" },
+    { HinhAnh: "./assets/img/slide4.jpg", Trailer: "https://www.youtube.com/watch?v=IIIgZI8QDc8" },
   ];
-  // dsPhim: any = [];
-  // srcTrailer: string = 'https://www.youtube.com/embed/Mh2ebPxhoLs';
-
+  
   // Option owl Carousel
   mySlideOptions = { items: 1, dots: true, nav: false, autoplay: true, loop: true };
   myCarouselOptions = { items: 3, dots: true, nav: true };
