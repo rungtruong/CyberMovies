4e75dd38a3ba50f7dc19770d2e30b858f0848aad
Forest Truong <forest.truong@styl.solutions>
2019-03-29 10:58:33 +0700

    fix tab search movie to book ticket

--- src/app/modules/home/tab-search-movie/tab-search-movie.component.ts
+++ src/app/modules/home/tab-search-movie/tab-search-movie.component.ts
@@ -3,6 +3,7 @@ import { MovieService } from 'src/app/_core/services/movie.service';
 import { formatDate } from '@angular/common';
 import Swal from 'sweetalert2';
 import { Router } from '@angular/router';
+import { TicketService } from 'src/app/_core/services/ticket.service';
 @Component({
   selector: 'app-tab-search-movie',
   templateUrl: './tab-search-movie.component.html',
@@ -11,12 +12,17 @@ import { Router } from '@angular/router';
 export class TabSearchMovieComponent implements OnInit, OnDestroy {
   comingsoon = "Phim sắp chiếu";
   movies: any;
+  selectMovie: any;
+  selectTheater: any;
+  selectDateShow: any;
+  selectTimeShow: any;
+  selectCodeShow: any;
   theaters: Array<any> = [];
   dates: Array<any> = [];
   times: Array<any> = [];
   selectmovieDetail: any = {};
   requestGetMovies: any;
-  constructor(private router:Router,private movieService: MovieService) { }
+  constructor(private router: Router, private movieService: MovieService, private ticketService: TicketService) { }
 
   ngOnInit() {
     this.getMoVies();
@@ -32,6 +38,7 @@ export class TabSearchMovieComponent implements OnInit, OnDestroy {
     })
   }
   movieDetail(movieCode) {
+    this.selectMovie = movieCode;
     this.theaters = [];
     this.dates = [];
     this.movieService.chiTietPhim(movieCode).subscribe(data => {
@@ -50,11 +57,13 @@ export class TabSearchMovieComponent implements OnInit, OnDestroy {
         this.theaters.push(this.selectmovieDetail.LichChieu[0]);
         this.dates.push(this.selectmovieDetail.LichChieu[0].NgayChieuGioChieu);
       }
-    
+
     });
 
   }
   movieTimeShow(time) {
+    this.selectDateShow = time;
+    time = formatDate(time, 'H:mm', 'en-US', '+07');
     this.times = [];
     for (let i = 0; i < this.dates.length; i++) {
       let currentTime = formatDate(this.dates[i], 'H:mm', 'en-US', '+07');
@@ -63,30 +72,61 @@ export class TabSearchMovieComponent implements OnInit, OnDestroy {
       }
     }
     this.times.push(time);
-    
+    this.getTicketDetailByDate();
   }
-  
-  muaVe(){
-    console.log(this.theaters);
-    if(this.times.length === 0)
-    {
-      Swal.fire('Thông báo','Bạn chưa chọn xuất chiếu','warning');
-      return;
+  chooseTheater(theater) {
+    this.selectTheater = theater;
+  }
+  chooseTimeShow(timeshow) {
+    this.selectTimeShow = timeshow;
+  }
+  validate() {
+    if (!localStorage.getItem("userLogin")) {
+      Swal.fire("Thông báo", "Vui lòng đăng nhập trước khi đặt vé", "warning");
+      return false;
     }
-    if(this.theaters.length === 0 )
-    {
-      Swal.fire('Thông báo','Phim chưa chiếu','warning');
-      return;
-
-    }else{
-    
-      let params = {
-        MaLichChieu: this.theaters[0].MaLichChieu,
-        MaPhim: this.theaters[0].MaPhim,
-        TenRap:this.theaters[0].Rap.TenRap,
-        NgayChieuGioChieu:this.theaters[0].NgayChieuGioChieu
+    if (!this.selectMovie) {
+      Swal.fire('Thông báo', 'Bạn chưa chọn phim muốn xem!', 'warning');
+      return false;
+    }
+    if (this.theaters.length === 0) {
+      Swal.fire('Thông báo', 'Phim sắp chiếu, mời bạn chọn phim khác!', 'warning');
+      return false;
+    } else {
+      if (!this.selectTheater) {
+        Swal.fire('Thông báo', 'Bạn chưa chọn rạp!', 'warning');
+        return false;
       }
-      this.router.navigate(['/bookticket'],{queryParams:params});
+    }
+    if (!this.selectDateShow) {
+      Swal.fire('Thông báo', 'Bạn chưa chọn ngày xem!', 'warning');
+      return false;
+    }
+    if (!this.selectTimeShow) {
+      Swal.fire('Thông báo', 'Bạn chưa chọn xuất chiếu!', 'warning');
+      return false;
+    }
+    return true;
+  }
+  getTicketDetailByDate() {
+    for (let i = 0; i < this.selectmovieDetail.LichChieu.length; i++) {
+      if (this.selectDateShow == this.selectmovieDetail.LichChieu[i].NgayChieuGioChieu) {
+        this.selectCodeShow = this.selectmovieDetail.LichChieu[i].MaLichChieu;
+      }
+    }
+  }
+  muaVe() {
+    // console.log(this.theaters);
+    if (this.validate()) {
+      let params = {
+        MaLichChieu: this.selectCodeShow,
+        MaPhim: this.selectMovie,
+        TenRap: this.selectTheater,
+        NgayChieuGioChieu: this.selectDateShow,
+      }
+      console.log(params);
+
+      this.router.navigate(['/bookticket'], { queryParams: params });
     }
   }
 }
