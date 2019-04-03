7ff4a7dd80560f6c6b3ec7bba85efd6c92db51fe
Forest Truong <forest.truong@styl.solutions>
2019-04-03 17:52:22 +0700

    renew movie detail component

--- src/app/modules/home/chitietphim/chitietphim.component.ts
+++ src/app/modules/home/chitietphim/chitietphim.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, OnChanges } from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { MovieService } from 'src/app/_core/services/movie.service';
 import * as $ from 'jquery';
@@ -10,51 +10,60 @@ import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
   styleUrls: ['./chitietphim.component.scss']
 })
 export class ChitietphimComponent implements OnInit {
-
-  phim: any = {LichChieu:[],MoTa:""};
+  stars: Array<any> = [];
+  stars_o: Array<any> = [];
+  isSelelect = 1;
+  phim: any = { LichChieu: [], MoTa: "" };
   autoPlay = false;
   today = new Date();
   tomorrow = new Date();
   videoUrl: SafeResourceUrl;
   currentUrl: any;
   private MaPhim;
-  private subParam : Subscription;
-  constructor( private activateRoute : ActivatedRoute, private movieService: MovieService,private sanitizer: DomSanitizer) { }
+  private subParam: Subscription;
+  constructor(private activateRoute: ActivatedRoute, private movieService: MovieService, private sanitizer: DomSanitizer) { }
 
   ngOnInit() {
     $('html, body').animate({
       scrollTop: $("body").offset().top - 70
-      }, 500);
-    this.subParam = this.activateRoute.params.subscribe((param)=>{
+    }, 500);
+    this.subParam = this.activateRoute.params.subscribe((param) => {
       this.MaPhim = param.id;
-
       //Gọi service lấy dưữ lịẹệu phim gan vao thuoc tinh phim binding len giao dien
       this.LayChiTietPhim(this.MaPhim);
-      
-      
     })
 
+
   }
-  scrollBookingList()
-  {
-    
+  scrollBookingList() {
+
     $('html, body').animate({
       scrollTop: $("#listSession").offset().top - 70
-      }, 500);
+    }, 500);
   }
- 
-  LayChiTietPhim(id:number)
-  {
+
+  LayChiTietPhim(id: number) {
     this.movieService.chiTietPhim(id).subscribe(res => {
       this.phim = res;
       console.log(this.phim)
+      this.renderStar(this.phim.DanhGia);
+      if (this.phim.LichChieu.length == 0) {
+        this.isSelelect = 2;
+      }
     })
   }
-  scrollTrailer()
-  {
+  scrollTrailer() {
     $('html, body').animate({
       scrollTop: $("#trailer").offset().top - 70
-      }, 500);
+    }, 500);
   }
 
+  public renderStar(star: number) {
+    for (let i = 0; i < star; i++) {
+      this.stars.push(i);
+    }
+    for (let i = 0; i < 5 - star; i++) {
+      this.stars_o.push(i);
+    }
+  }
 }
